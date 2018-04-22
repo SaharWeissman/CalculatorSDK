3596cbefe9017a3f9b66b132c421e859dcf7b7e2
Sahar Weissman <saharweissman@gmail.com>
2018-04-22 20:16:07 +0300

    Initial commit

---
+++ ConsoleApp/src/com/saharw/calculator/console/Main.kt
@@ -0,0 +1,11 @@
+package com.saharw.calculator.console
+
+import core.Calculator
+
+/**
+ * Created by Sahar on 04/22/2018.
+ */
+fun main(args:Array<String>) {
+    var calc = Calculator("Hello world!")
+    calc.printTest()
+}
---
+++ Lib/src/core/Calculator.kt
@@ -0,0 +1,11 @@
+package core
+
+/**
+ * Created by Sahar on 04/22/2018.
+ */
+class Calculator(val test: String) {
+
+    fun printTest() {
+        println("test: $test")
+    }
+}
